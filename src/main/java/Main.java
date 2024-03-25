d2a8b149e99c696945d50b3a70235e80f43ca956
Prachi Pawaskar <pawaskar.a.prachi@gmail.com>
2024-03-25 16:36:41 +0530

    Update Main.java
    Changed sysout

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,6 +1,6 @@
 public class Main {
     public static void main(String args[])  //static method
     {
-        System.out.println("Static method one");
+        System.out.println("First main method");
     }
 }
