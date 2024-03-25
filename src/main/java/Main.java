7f95ce73bb7d705ab939baf58015f6983110f79b
Prachi Pawaskar <pawaskar.a.prachi@gmail.com>
2024-03-25 16:33:53 +0530

    Update Main.java
    Changed main 1 print message

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,6 +1,6 @@
 public class Main {
     public static void main(String args[])  //static method
     {
-        System.out.println("Static method");
+        System.out.println("Static method one");
     }
 }
